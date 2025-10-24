51a784cc4ee5fb43dcadb93a5519171494c0de93
NadiiaSavchuk2210 <nadiia.savchuk.g@gmail.com>
2025-10-24 22:09:41 +0300

    convert SidebarNotes to a server component

--- app/notes/filter/@sidebar/default.tsx
+++ app/notes/filter/@sidebar/default.tsx
@@ -1,24 +1,16 @@
-'use client';
-
 import Link from 'next/link';
 import css from './page.module.css';
 import { NOTE_TAGS } from '../../../../constants';
-import { useParams } from 'next/navigation';
-import clsx from 'clsx';
 
-
-const SidebarNotes = () => {
-    const { slug } = useParams<{ slug: string[] }>();
-    const tag = slug?.[0];
-
+const SidebarNotes = async () => {
     return (
         <ul className={css.menuList}>
             <li className={css.menuItem}>
-                <Link href={`/notes/filter/all`} className={clsx(css.menuLink, tag === 'all' && css.menuLinkActive)}>All notes</Link>
+                <Link href={`/notes/filter/all`} className={css.menuLink}>All notes</Link>
             </li>
             {NOTE_TAGS.map(noteTag => (
                 <li className={css.menuItem} key={noteTag}>
-                    <Link href={`/notes/filter/${noteTag}`} className={clsx(css.menuLink, tag === noteTag && css.menuLinkActive)}>{noteTag}</Link>
+                    <Link href={`/notes/filter/${noteTag}`} className={css.menuLink}>{noteTag}</Link>
                 </li>
             ))}
         </ul>
